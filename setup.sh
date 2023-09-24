afbbc485b180b94c80857a9cb7ef34fb4eeeb31c
Yuval Saban <62604022+YuvalSab@users.noreply.github.com>
2023-09-24 15:26:51 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ sudo apt update
 sudo apt install jq -y
 sudo apt install apt-transport-https ca-certificates gnupg curl sudo -y
 rm -f /etc/apt/sources.list.d/google-cloud-sdk.list ; echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - ; sudo apt update
 sudo apt install google-cloud-cli -y
 chmod +x ./GCP_ServiceAccountsEnum.sh
 chmod +x ./GCP_ComputeInstancesEnum.sh
