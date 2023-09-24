2b2d227b2c84b44c108eb1eabee77de5012500cb
Yuval Saban <62604022+YuvalSab@users.noreply.github.com>
2023-09-24 15:34:42 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install jq -y
-sudo apt install apt-transport-https ca-certificates gnupg curl sudo -y
-rm -f /etc/apt/sources.list.d/google-cloud-sdk.list ; echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo apt update
-sudo apt install google-cloud-cli -y
-chmod +x ./GCP_ServiceAccountsEnum.sh
-chmod +x ./GCP_ComputeInstancesEnum.sh
+sudo apt update  2>/dev/null &&
+sudo apt install jq -y 2>/dev/null &&
+sudo apt install apt-transport-https ca-certificates gnupg curl sudo -y 2>/dev/null &&
+rm -f /etc/apt/sources.list.d/google-cloud-sdk.list ; echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list 2>/dev/null &&
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - 2>/dev/null &&
+sudo apt update 2>/dev/null &&
+sudo apt install google-cloud-cli -y 2>/dev/null &&
+chmod +x ./GCP_ServiceAccountsEnum.sh 2>/dev/null &&
+chmod +x ./GCP_ComputeInstancesEnum.sh 2>/dev/null
