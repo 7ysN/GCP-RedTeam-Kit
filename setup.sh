e9250589ff1f88b8d1892db6fbf7b36a50c3f4ba
ysN <62604022+7ysN@users.noreply.github.com>
2023-12-25 17:18:26 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 echo "\e[5;32m[+] Running Setup: \e[0m"
-sudo apt update  2>/dev/null &&
+sudo apt update -y 2>/dev/null &&
 sudo apt install jq -y 2>/dev/null &&
 sudo apt install apt-transport-https ca-certificates gnupg curl sudo -y 2>/dev/null &&
 rm -f /etc/apt/sources.list.d/google-cloud-sdk.list ; echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list 2>/dev/null &&
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - 2>/dev/null &&
-sudo apt update 2>/dev/null &&
+sudo apt update -y 2>/dev/null &&
 sudo apt install google-cloud-cli -y 2>/dev/null &&
 chmod +x ./GCP_ServiceAccountsEnum.sh 2>/dev/null &&
 chmod +x ./GCP_ComputeInstancesEnum.sh 2>/dev/null &&
